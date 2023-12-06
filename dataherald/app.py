c5022bd2eb4973510d0ea130e7a56f0a1260dcf1
James Patten <jamespatten@192.168.1.117>
2023-12-06 21:14:54 +0000

    added cors middleware

--- dataherald/app.py
+++ dataherald/app.py
@@ -1,7 +1,25 @@
 import dataherald
 import dataherald.config
 from dataherald.server.fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 settings = dataherald.config.Settings()
 server = FastAPI(settings)
 app = server.app()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://airpipe.onrender.com",
+    "https://api-airpipe.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
